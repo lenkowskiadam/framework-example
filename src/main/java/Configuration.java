f629a086a868f872d7eeef4bf50cf84a43c2e071
adamlenkowski <lenkowski.adam@gmail.com>
2022-08-24 19:31:21 +0200

    rup up

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+
+    private static String getEnvironment() {
+        return System.getProperty("env");
+    }
+
+    public static Properties getProperties() {
+        Properties properties = new Properties();
+        try {
+            InputStream input = Configuration.class.getClassLoader().getResourceAsStream(String.format("dev.properties", getEnvironment()));
+            if (input == null) {
+                System.out.println("Cannot find file");
+            }
+            properties.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    public static String getUrl() {
+        return getProperties().getProperty("url");
+    }
+}
